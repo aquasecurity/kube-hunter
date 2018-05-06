cd880ec50ee4304b24064800ce4e44c873a1b887
Shir <shirtam7@gmail.com>
2018-05-06 19:50:00 +0300

    Delete everything of Kube Hunter 1.0

--- hunters/kubelet.py
+++
@@ -1,9 +0,0 @@
-from hunters.hunter import Hunter
-
-
-class Kubelet(Hunter):
-    def __init__(self, host):
-        self.host = host
-
-    def hunt(self, *args, **kwargs):
-        raise NotImplementedError()
