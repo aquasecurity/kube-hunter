7041280f2c7457fcdc45141861cfc266e0a3daed
ori.agmon <ori.agmon@aquasec.com>
2018-10-10 19:11:04 +0300

    Removed the helpers functions

--- src/modules/hunting/etcd.py
+++ src/modules/hunting/etcd.py
@@ -7,37 +7,6 @@ from ...core.events import handler
 from ...core.events.types import Vulnerability, Event, OpenPortEvent
 from ...core.types import ActiveHunter, Hunter, KubernetesCluster, InformationDisclosure, RemoteCodeExec, UnauthenticatedAccess, AccessRisk
 
-""" Helper functions """
-
-# Will attempt to do request "req1" with the optional parameters.
-# If fails it will attempt to do "req2" with the optional parameters.
-# If once of the request success this method will return True, if both fail- False.
-def helperFuncDo2Requests(req1, req2, is_verify=False, data=None, req_type="get"):
-    try:
-        r = helperDoRequest(req1, is_verify, data, req_type)
-        has_remote_access_gained = (r.status_code == 200 and r.content != "")
-        if has_remote_access_gained:
-            return r
-    except Exception:
-        try:
-            r = helperDoRequest(req2, is_verify, data, req_type)
-            has_remote_access_gained = (r.status_code == 200 and r.content != "")
-            if has_remote_access_gained:
-                return r
-        except Exception:
-            return False  # None of the requests succeded..
-    return False
-
-
-def helperDoRequest(req, is_verify, data=None, req_type="get"):
-    if req_type == "put":
-        r = requests.put(req, verify=is_verify, timeout=3, data=data)
-        return r
-    elif req_type == "get":
-        r = requests.get(req, verify=is_verify, timeout=3, data=data)
-        return r
-
-
 """ Vulnerabilities """
 class EtcdRemoteWriteAccessEvent(Vulnerability, Event):
     """Remote write access might grant an attacker full control over the kubernetes cluster"""
@@ -81,13 +50,9 @@ class EtcdRemoteAccessActive(ActiveHunter):
         data = {
             'value': 'remotely written data'
         }
-        r_secure = "https://{host}:{port}/v2/keys/message".format(host=self.event.host, port=2379)
-        r_not_secure = "http://{host}:{port}/v2/keys/message".format(host=self.event.host, port=2379)
-        res = helperFuncDo2Requests(r_secure, r_not_secure)
-        if res:
-            self.write_evidence = res.content
-            return True
-        return False
+        r = "{protocol}://{host}:{port}/v2/keys/message".format(host=self.event.host, port=2379, protocol=self.protocol, data=data)
+        self.write_evidence = r.content if r.status_code == '200' and r.content != '' else False
+        return self.write_evidence
 
     def execute(self):
         if self.db_keys_write_access():
@@ -99,48 +64,35 @@ class EtcdRemoteAccess(Hunter):
     """Etcd Remote Access
     Checks for remote availability of etcd, version, read access, write access
     """
-    # TODO:
-    # Check the etcd hunter on a remote cluster! (currently everything was checked only at 127.0.0.1:2379)
     def __init__(self, event):
         self.event = event
         self.version_evidence = ''
         self.keys_evidence = ''
+        self.protocol = 'https'
 
     def db_keys_disclosure(self):
-        logging.debug(self.event.host)
-        logging.debug("Passive hunter is attempting to read etcd keys remotely")
-        r_secure = "https://{host}:{port}/v2/keys".format(host=self.event.host, port=2379)
-        r_not_secure = "http://{host}:{port}/v2/keys".format(host=self.event.host, port=2379)
-        res = helperFuncDo2Requests(r_secure, r_not_secure)
-        if res:
-            self.keys_evidence = res.content
-            return True
-        return False
+        logging.debug(self.event.host + " Passive hunter is attempting to read etcd keys remotely")
+        r = requests.get("{protocol}://{host}:{port}/v2/keys".format(protocol=self.protocol, host=self.event.host, port=2379), verify=False)
+        self.keys_evidence = r.content if r.status_code == '200' and r.content != '' else False
+        return self.version_evidence
 
-    def version_disclosure(self):
-        logging.debug(self.event.host)
-        logging.debug("Passive hunter is attempting to check etcd version remotely")
-        r_secure = "https://{host}:{port}/version".format(host=self.event.host, port=2379)
-        r_not_secure = "http://{host}:{port}/version".format(host=self.event.host, port=2379)
-        res = helperFuncDo2Requests(r_secure, r_not_secure)
-        if res:
-            self.version_evidence = res.content
-            return True
-        return False
+    def version_disclosure(self, protocol):
+        logging.debug(self.event.host + " Passive hunter is attempting to check etcd version remotely")
+        r = requests.get("{protocol}://{host}:{port}/version".format(protocol=self.protocol, host=self.event.host, port=2379), verify=False)
+        self.version_evidence = r.content if r.status_code == '200' and r.content != '' else False
+        return self.version_evidence
 
     def unauthorized_access(self):
-        logging.debug(self.event.host)
-        logging.debug("Passive hunter is attempting to access etcd without authorization")
-        r_not_secure = "http://{host}:{port}/version".format(host=self.event.host, port=2379)
-        res = helperFuncDo2Requests(r_not_secure, r_not_secure)  # We don't have to do 2 requests this time
-        if res:
-            return True
-        return False
+        logging.debug(self.event.host + " Passive hunter is attempting to access etcd without authorization")
+        r = requests.get("http://{host}:{port}/version".format(host=self.event.host, port=2379), verify=False)
+        return r.content if r.status_code == '200' and r.content != '' else False
 
     def execute(self):
+        if self.unauthorized_access():  # inits http/https protocol
+            self.protocol = 'http'
         if self.version_disclosure():
             self.publish_event(EtcdRemoteVersionDisclosureEvent(self.version_evidence))
-            if self.unauthorized_access():
+            if self.protocol == 'http' and self.unauthorized_access():
                 self.publish_event(EtcdAccessEnabledWithoutAuthEvent(self.version_evidence))
             if self.db_keys_disclosure():
                 self.publish_event(EtcdRemoteReadAccessEvent(self.keys_evidence))
