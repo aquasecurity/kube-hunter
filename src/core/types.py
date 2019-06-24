35fac148eab576a373cfecaca887e233800d7cc6
Daniel Sagi <danielsagi2009@gmail.com>
2019-06-24 22:30:44 +0300

    added KubectlClient component

--- src/core/types.py
+++ src/core/types.py
@@ -37,6 +37,9 @@ class KubernetesCluster():
     """Kubernetes Cluster"""
     name = "Kubernetes Cluster"
 
+class KubectlClient():
+    """The kubectl client binary is used by the user, to interact with the cluster"""
+    name = "Kubectl Client"
 
 class Kubelet(KubernetesCluster):
     """The kubelet is the primary "node agent" that runs on each node"""
