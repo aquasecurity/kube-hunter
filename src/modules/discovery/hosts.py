2c8e8467d2ce54e9ce46afc4260f65f64aa1dc71
Daniel Sagi <danielsagi2009@gmail.com>
2019-07-01 20:14:45 +0300

    refactored most, added RunningPodOnCloud event, triggered on cloud detection, also added support for the new CloudTypes

--- src/modules/discovery/hosts.py
+++ src/modules/discovery/hosts.py
@@ -14,7 +14,11 @@ from netifaces import AF_INET, ifaddresses, interfaces, gateways
 
 from ...core.events import handler
 from ...core.events.types import Event, NewHostEvent, Vulnerability
-from ...core.types import Discovery, InformationDisclosure, Azure
+from ...core.types import Discovery, InformationDisclosure, Azure, CloudTypes
+
+class RunningPodOnCloud(Event):
+    def __init__(self, cloud):
+        self.cloud = cloud
 
 class RunningAsPodEvent(Event):
     def __init__(self):
@@ -37,55 +41,41 @@ class RunningAsPodEvent(Event):
         except IOError:
             pass
 
-class AzureMetadataApi(Vulnerability, Event):
-    """Access to the Azure Metadata API exposes information about the machines associated with the cluster"""
-    def __init__(self, cidr):
-        Vulnerability.__init__(self, Azure, "Azure Metadata Exposure", category=InformationDisclosure)
-        self.cidr = cidr
-        self.evidence = "cidr: {}".format(cidr)
-
 class HostScanEvent(Event):
-    def __init__(self, pod=False, active=False, predefined_hosts=list()):
-        self.active = active # flag to specify whether to get actual data from vulnerabilities
-        self.predefined_hosts = predefined_hosts
-
+    pass
 
 # for comparing prefixes
 class InterfaceTypes(Enum):
     LOCALHOST = "127"
 
-class CloudTypes(Enum):
-    AZURE = "Azure"
-
-
 class HostDiscoveryUtils:
     """ Static class containes util functions for Host discovery processes """
     @classmethod
-    def get_cloud(cls, host=None):
+    def get_cloud(cls, host):
         """ Returns cloud for a given ip address, defaults to the external ip"""
-        if not host:
-            host = cls._get_external_ip()
+        cloud = ""
         try:
             if host:
-                logging.debug("Checking whether the cluster is deployed on azure's cloud")
+                logging.debug("Checking if {} is deployed on a cloud".format(host))
                 # azurespeed.com provide their API via HTTP only; the service can be queried with 
                 # HTTPS, but doesn't show a proper certificate. Since no encryption is worse then
                 # any encryption, we go with the verify=false option for the time being. At least
                 # this prevents leaking internal IP addresses to passive eavesdropping.
                 # TODO: find a more secure service to detect cloud IPs
                 metadata = requests.get("https://www.azurespeed.com/api/region?ipOrUrl={ip}".format(ip=host), verify=False).text
+                if "cloud" in metadata:
+                    cloud = json.loads(metadata)["cloud"]
         except requests.ConnectionError as e:
             logging.info("- unable to check cloud: {0}".format(e))
-            return
-        if "cloud" in metadata:
-            return json.loads(metadata)["cloud"]
+
+        return CloudTypes.get_enum(cloud)
 
     @staticmethod 
     def get_default_gateway():
         return gateways()['default'][AF_INET][0]
 
     @staticmethod
-    def _get_external_ip():
+    def get_external_ip():
         external_ip = None
         try:
             logging.debug("HostDiscovery hunter attempting to get external IP address")
@@ -112,7 +102,6 @@ class HostDiscoveryUtils:
                     continue
                 for ip in HostDiscoveryUtils.generate_subnet(ip, sn):
                     yield ip
-                    
 
 
 @handler.subscribe(RunningAsPodEvent)
@@ -124,52 +113,32 @@ class FromPodHostDiscovery(Discovery):
         self.event = event
 
     def execute(self):
-        scan_subnets = list()
-        # Scan any hosts that the user specified
+        # Scan any additional hosts that the user specified
         if config.remote or config.cidr:
             self.publish_event(HostScanEvent())
-        else:
-            # TODO: add more api discoveries
-            # If we have access to azure's api, we extract subnets from there 
-            if self.is_azure_api():
-                subnets, cloud = self.azure_metadata_discovery()
-                scan_subnets += subnets
 
-            subnets, cloud = self.pod_discovery()
-            scan_subnets += subnets
+        # figuring out the cloud from the external ip, default to CloudTypes.NO_CLOUD
+        external_ip = HostDiscoveryUtils.get_external_ip()
+        cloud = HostDiscoveryUtils.get_cloud(external_ip)
 
-            for subnet in scan_subnets:
-                logging.debug("From pod scanning subnet {0}/{1}".format(subnet[0], subnet[1]))
-                for ip in HostDiscoveryUtils.generate_subnet(ip=subnet[0], sn=subnet[1]):
-                    self.publish_event(NewHostEvent(host=ip, cloud=cloud))
-    
-    def is_azure_api(self):
-        try:
-            logging.debug("From pod attempting to access Azure Metadata API")
-            if requests.get("http://169.254.169.254/metadata/instance?api-version=2017-08-01", headers={"Metadata":"true"}, timeout=5).status_code == 200:
-                return True
-        except requests.exceptions.ConnectionError:
-            return False
+        # specific cloud discoveries should subscribe to RunningPodOnCloud
+        if cloud != CloudTypes.NO_CLOUD:
+            self.publish_event(RunningPodOnCloud(cloud=cloud))
 
-    def pod_discovery(self):
+        # normal pod discovery
+        scan_subnets = self.pod_subnet_discovery()
+        for subnet in scan_subnets:
+            logging.debug("From pod scanning subnet {0}/{1}".format(subnet[0], subnet[1]))
+            for ip in HostDiscoveryUtils.generate_subnet(ip=subnet[0], sn=subnet[1]):
+                self.publish_event(NewHostEvent(host=ip, cloud=CloudTypes.NO_CLOUD))
+
+    def pod_subnet_discovery(self):
         # normal option when running as a pod is to scan it's own subnet
         # The gateway connects us to the host, and we can discover the 
         # kubelet from there, other ip's are pods that are running 
-        # next to us
-        return [(HostDiscoveryUtils.get_default_gateway(), "24")], HostDiscoveryUtils.get_cloud()
-        
-    # quering azure's interface metadata api | works only from a pod
-    def azure_metadata_discovery(self):
-        logging.debug("From pod attempting to access azure's metadata")
-        machine_metadata = json.loads(requests.get("http://169.254.169.254/metadata/instance?api-version=2017-08-01", headers={"Metadata":"true"}).text)
-        subnets = list()
-        for interface in machine_metadata["network"]["interface"]:
-            address, subnet = interface["ipv4"]["subnet"][0]["address"], interface["ipv4"]["subnet"][0]["prefix"]
-            logging.debug("From pod discovered subnet {0}/{1}".format(address, subnet if not config.quick else "24"))
-            subnets.append([address,subnet if not config.quick else "24"])
-            self.publish_event(AzureMetadataApi(cidr="{}/{}".format(address, subnet)))
+        # next to us,
+        return [(HostDiscoveryUtils.get_default_gateway(), "24")]
 
-        return subnets, CloudTypes.AZURE.value
 
 @handler.subscribe(HostScanEvent)
 class HostDiscovery(Discovery):
@@ -180,6 +149,7 @@ class HostDiscovery(Discovery):
         self.event = event
 
     def execute(self):
+        # handling multiple scan options
         if config.cidr:
             try:
                 ip, sn = config.cidr.split('/')
@@ -189,14 +159,15 @@ class HostDiscovery(Discovery):
             cloud = HostDiscoveryUtils.get_cloud(ip)
             for ip in HostDiscoveryUtils.generate_subnet(ip, sn=sn):
                 self.publish_event(NewHostEvent(host=ip, cloud=cloud))                
-        elif config.internal:
+        if config.internal:
             self.scan_interfaces()
-        elif len(config.remote) > 0:
+        if len(config.remote) > 0:
             for host in config.remote:
                 self.publish_event(NewHostEvent(host=host, cloud=HostDiscoveryUtils.get_cloud(host)))
  
     # for normal scanning
     def scan_interfaces(self):
-        cloud = HostDiscoveryUtils.get_cloud()
+        external_ip = HostDiscoveryUtils.get_external_ip()
+        cloud = HostDiscoveryUtils.get_cloud(host=external_ip)
         for ip in HostDiscoveryUtils.generate_interfaces_subnet():
             handler.publish_event(NewHostEvent(host=ip, cloud=cloud))
