e97c501cf2796d20ae2ce50bd7e67cf6e363ab0c
Daniel Sagi <danielsagi2009@gmail.com>
2019-08-12 16:12:27 +0300

    improved discovery, and added KNOWN PORTS usage

--- src/modules/discovery/apiserver.py
+++ src/modules/discovery/apiserver.py
@@ -7,6 +7,8 @@ from ...core.events import handler
 from ...core.events.types import OpenPortEvent, Service, Event
 
 
+KNOWN_API_PORTS = [443, 6443, 8080]
+
 class ApiServer(Service, Event):
     """The API server is in charge of all operations on the cluster."""
     def __init__(self, protocol="https"):
@@ -21,7 +23,7 @@ class MetricsServer(Service, Event):
  
 # Other devices could have this port open, but we can check to see if it looks like a Kubernetes node
 # A Kubernetes API server will to respond to a get to /version 
-@handler.subscribe(OpenPortEvent, predicate=lambda x: x.port==443 or x.port==6443 or x.port==8080 and x.kubeservicehost and x.kubeservicehost != x.host)
+@handler.subscribe(OpenPortEvent, predicate=lambda x: x.port in KNOWN_API_PORTS)
 class ApiServerDiscovery(Discovery):
     """API Server Discovery
     Checks for the existence of a an API Server
@@ -32,7 +34,7 @@ class ApiServerDiscovery(Discovery):
     def execute(self):
         # if were running as a pod, we only want to discover the api server from the env variable.
         if self.event.kubeservicehost and self.event.kubeservicehost != self.event.host:
-            pass
+            return
         logging.debug("Attempting to discover an API server on {}:{}".format(self.event.host, self.event.port))
         self.make_request(protocol="https")
         self.make_request(protocol="http")    
@@ -50,7 +52,7 @@ class ApiServerDiscovery(Discovery):
 
 
 # Making sure to not subscribe to the Api Server's host
-@handler.subscribe(OpenPortEvent, predicate=lambda x: x.port==443 or x.port==6443 or x.port==8080 and x.kubeservicehost != x.host)
+@handler.subscribe(OpenPortEvent, predicate=lambda x: x.port in KNOWN_API_PORTS and x.kubeservicehost != x.host)
 class MetricsServerDiscovery(Discovery):
     """Metrics Server Discovery
     Checks for the existence of a a Metrics Server Pod
@@ -62,17 +64,9 @@ class MetricsServerDiscovery(Discovery):
         r = requests.get("https://{}:{}/version".format(self.event.host, self.event.port), verify=False)
         try:
             versions = json.loads(r.text)
+            # major version on a metrics server will return empty
+            if versions.get("major") == "":
+                self.publish_event(MetricsServer())
         except Exception as e:
             logging.error("Could not access /version on metrics server at: {}".format(e))
-        
-    def make_request(self, protocol):
-        try:
-            r = requests.get("{}://{}:{}".format(protocol, self.event.host, self.event.port), verify=False)
-            if ('k8s' in r.text) or ('"code"' in r.text and r.status_code is not 200): 
-                self.event.role = "Master"
-                self.publish_event(ApiServer(protocol=protocol))
-        except requests.exceptions.SSLError:
-            logging.debug("{} protocol not accepted on {}:{}".format(protocol, self.event.host, self.event.port))
-        except Exception as e:
-            logging.debug("{} on {}:{}".format(e, self.event.host, self.event.port))
-
+            
