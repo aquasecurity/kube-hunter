56453d01e290680dcd727c3f3e4b1833559800c0
Yehuda Chikvashvili <yehudaac1@gmail.com>
2019-10-18 18:21:12 +0300

    Consider patched versions as not vulnerable by default
    Change `--ignore-downstream` to `--ignore-patched-versions` and
    invert it's effect.
    From now on, kube-hunter will not alert patched components as default
    behavior.
    
    Resolves #194

--- runtest.py
+++ runtest.py
@@ -6,7 +6,7 @@ parser.add_argument('--list', action="store_true", help="displays all tests in k
 parser.add_argument('--interface', action="store_true", help="set hunting of all interface network interfaces")
 parser.add_argument('--pod', action="store_true", help="set hunter as an insider pod")
 parser.add_argument('--quick', action="store_true", help="Prefer quick scan (subnet 24)")
-parser.add_argument('--ignore-downstream', action="store_true", help="Ignore patched kubernetes versions")
+parser.add_argument('--ignore-patched-versions', action="store_true", help="Ignore patches in version comparisons")
 parser.add_argument('--cidr', type=str, help="set an ip range to scan, example: 192.168.0.0/16")
 parser.add_argument('--mapping', action="store_true", help="outputs only a mapping of the cluster's nodes")
 parser.add_argument('--remote', nargs='+', metavar="HOST", default=list(), help="one or more remote ip/dns to hunt")
--- tests/hunting/test_cvehunting.py
+++ tests/hunting/test_cvehunting.py
@@ -20,16 +20,7 @@ def test_K8sCveHunter():
     assert cve_counter == 2
     cve_counter = 0
 
-    # test complex version
-    e = K8sVersionDisclosure(version="1.10.1-gke-1", from_endpoint="/version")
-    h = K8sClusterCveHunter(e)
-    h.execute()
-
-    time.sleep(0.01)
-    assert cve_counter == 2
-    cve_counter = 0
-
-    # test complex version
+    # test patched version
     e = K8sVersionDisclosure(version="v1.13.6-gke.13", from_endpoint="/version")
     h = K8sClusterCveHunter(e)
     h.execute()
