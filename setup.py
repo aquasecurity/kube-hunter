e75c0ff37b12b66a33735374d1509c3660be3135
Yehuda Chikvashvili <yehudaac1@gmail.com>
2020-02-18 17:31:10 +0200

    Add PyInstaller build (#302)
    * Add PyInstaller build
    
    Use PyInstaller to generate single binary.
    Use staticx to generate a single static binary.
    
    Resolves #301
    
    * Add test Makefile target
    
    Add test to Makefile.
    Add requests_mock to dev dependencies.

--- setup.py
+++ setup.py
@@ -1,5 +1,41 @@
-from setuptools import setup
+from subprocess import check_call
+from pkg_resources import parse_requirements
+from configparser import ConfigParser
+from setuptools import setup, Command
+
+
+class PyInstallerCommand(Command):
+    """A custom command to run PyInstaller to build standalone executable."""
+
+    description = "run PyInstaller on kube-hunter entrypoint"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        cfg = ConfigParser()
+        cfg.read("setup.cfg")
+        command = [
+            "pyinstaller",
+            "--clean",
+            "--onefile",
+            "--name",
+            "kube-hunter",
+        ]
+        setup_cfg = cfg["options"]["install_requires"]
+        requirements = parse_requirements(setup_cfg)
+        for r in requirements:
+            command.extend(["--hidden-import", r.key])
+        command.append("kube_hunter/__main__.py")
+        print(' '.join(command))
+        check_call(command)
+
 
 setup(
     use_scm_version=True,
+    cmdclass={"pyinstaller": PyInstallerCommand},
 )
