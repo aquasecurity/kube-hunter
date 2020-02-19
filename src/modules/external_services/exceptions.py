39d6284712d2fd7a8feaaed7bc9e9674b7a21150
Raito Bezarius <masterancpp@gmail.com>
2020-02-19 23:48:59 +0100

    PR: address comments

--- src/modules/external_services/exceptions.py
+++ src/modules/external_services/exceptions.py
@@ -1,7 +1,15 @@
 class ExternalServiceFailure(Exception):
+    """
+    This exception can be thrown when a service action has failed (e.g. unable to determine any IP address, no cache result available when offline)
+    It is for user-code and external consumption of the service.
+    """
     def __init__(self):
         pass
 
 class ExternalProviderFailure(Exception):
+    """
+    This exception can be thrown when the provider of the service fails (e.g. the site is down, 400 error, etc.)
+    It is for internal consumption of external services which will decide to act upon a provider failure, by providing a cached result or throw a service failure.
+    """
     def __init__(self, exc=None):
         self.wrapped_exc = exc
--- src/modules/external_services/external_ip.py
+++ src/modules/external_services/external_ip.py
@@ -11,37 +11,41 @@ def generic_text_http_provider(url):
     except requests.ConnectionError as e:
         raise ExternalProviderFailure(e)
 
-ExtIPProvider = namedtuple('ExternalIPProvider', [
-    'handler',
+ExternalIPProvider = namedtuple('ExternalIPProvider', [
+    'provide', # provide the current address IP.
     'priority' # it means: secure, no logs, etc.
 ])
 
-CanHazIP = ExtIPProvider(handler=generic_text_http_provider('https://canhazip.com'), priority=0)
-Ipify = ExtIPProvider(handler=generic_text_http_provider('https://api.ipify.org'), priority=1)
+CanHazIP = ExternalIPProvider(provide=generic_text_http_provider('https://canhazip.com'), priority=0)
+Ipify = ExternalIPProvider(provide=generic_text_http_provider('https://api.ipify.org'), priority=1)
 
 DEFAULT_PROVIDERS = [CanHazIP, Ipify]
 
 class ExternalIPService:
     def __init__(self, providers=None, cache_results=True):
-        if providers and not all(isinstance(i, ExtIPProvider) for i in providers):
+        if providers and not all(isinstance(i, ExternalIPProvider) for i in providers):
             raise TypeError('A provider must be of type `ExternalIPProvider`!')
-        
+
         # Order provider by priority
         self.providers = sorted(providers or DEFAULT_PROVIDERS, key=lambda provider: provider.priority, reverse=True)
         self.cache_results = cache_results
+        self.cached_ip = None
 
-    def try_get(self, bypass_cache=False):
-        if not bypass_cache and self.cache_results and self.cached_ip:
+    def try_get(self, use_cache=True):
+        if use_cache and self.cache_results and self.cached_ip:
             return self.cached_ip
-        
+
         last_exc = None
         for provider in self.providers:
             try:
-                ip = provider.handler()
-                self.cached_ip = ip
+                ip = provider.provide()
+
+                if self.cache_results:
+                    self.cached_ip = ip
+
                 return ip
             except ExternalProviderFailure as exc:
                 last_exc = exc
-        
+
         # Tough luck.
         raise ExternalServiceFailure(exc=last_exc)
