aquasecurity/kube-hunter

commit df12d75d6dcba8d9415212f2cc5a9c8d107409bf

Packaging Kube-Hunter for PyPi (#272)
* Inital Commit

Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

* Suggestions implemented as suggested

Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

* Package with setuptools

Use setuptools to package kube-hunter as redistributable file.
Once packages, it can be pushed to PyPi.
The package version is taken from git tags (using setuptools_scm).

Closes #185

* Ignore __main__.py script in code coverage

The entrypoint script should not be tested but rather be calling
to tested modules.
Ideally, __main__ should only make a call to single function from
another tested module.

* Update requirements files

Use install_requires from setup.cfg file as single source of truth
for dependencies.
Install regular dependencies when installing dev dependencies.

* Symlink kube-hunter.py to entry point

Support the old way to run kube-hunter via the main script by making
a link to the new kube_hunter/__main__.py script.

Co-authored-by: Yehuda Chikvashvili <yehudaac1@gmail.com>

.gitignore
@@ -7,6 +7,23 @@ venv/
 .coverage
 .idea
 
+# Distribution / packaging
+.Python
+env/
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+*.egg-info/
+.installed.cfg
+*.egg
+
 # Directory Cache Files
 .DS_Store
 thumbs.db

kube-hunter.py
@@ -1,130 +1,1 @@
-#!/usr/bin/env python3
-
-import argparse
-import logging
-import threading
-
-from kube_hunter.conf import config
-from kube_hunter.modules.report.plain import PlainReporter
-from kube_hunter.modules.report.yaml import YAMLReporter
-from kube_hunter.modules.report.json import JSONReporter
-from kube_hunter.modules.report.dispatchers import STDOUTDispatcher, HTTPDispatcher
-from kube_hunter.core.events import handler
-from kube_hunter.core.events.types import HuntFinished, HuntStarted
-from kube_hunter.modules.discovery.hosts import RunningAsPodEvent, HostScanEvent
-
-
-loglevel = getattr(logging, config.log.upper(), logging.INFO)
-
-if config.log.lower() != "none":
-    logging.basicConfig(level=loglevel, format='%(message)s', datefmt='%H:%M:%S')
-
-reporters = {
-    'yaml': YAMLReporter,
-    'json': JSONReporter,
-    'plain': PlainReporter
-}
-
-if config.report.lower() in reporters.keys():
-    config.reporter = reporters[config.report.lower()]()
-else:
-    logging.warning('Unknown reporter selected, using plain')
-    config.reporter = reporters['plain']()
-
-dispatchers = {
-    'stdout': STDOUTDispatcher,
-    'http': HTTPDispatcher
-}
-
-if config.dispatch.lower() in dispatchers.keys():
-    config.dispatcher = dispatchers[config.dispatch.lower()]()
-else:
-    logging.warning('Unknown dispatcher selected, using stdout')
-    config.dispatcher = dispatchers['stdout']()
-
-import kube_hunter
-
-
-def interactive_set_config():
-    """Sets config manually, returns True for success"""
-    options = [("Remote scanning", "scans one or more specific IPs or DNS names"),
-    ("Interface scanning","scans subnets on all local network interfaces"),
-    ("IP range scanning","scans a given IP range")]
-
-    print("Choose one of the options below:")
-    for i, (option, explanation) in enumerate(options):
-        print("{}. {} ({})".format(i+1, option.ljust(20), explanation))
-    choice = input("Your choice: ")
-    if choice == '1':
-        config.remote = input("Remotes (separated by a ','): ").replace(' ', '').split(',')
-    elif choice == '2':
-        config.interface = True
-    elif choice == '3':
-        config.cidr = input("CIDR (example - 192.168.1.0/24): ").replace(' ', '')
-    else:
-        return False
-    return True
-
-
-def list_hunters():
-    print("\nPassive Hunters:\n----------------")
-    for hunter, docs in handler.passive_hunters.items():
-        name, doc = hunter.parse_docs(docs)
-        print("* {}\n  {}\n".format(name, doc))
-
-    if config.active:
-        print("\n\nActive Hunters:\n---------------")
-        for hunter, docs in handler.active_hunters.items():
-            name, doc = hunter.parse_docs(docs)
-            print("* {}\n  {}\n".format( name, doc))
-
-
-global hunt_started_lock
-hunt_started_lock = threading.Lock()
-hunt_started = False
-
-def main():
-    global hunt_started
-    scan_options = [
-        config.pod,
-        config.cidr,
-        config.remote,
-        config.interface
-    ]
-    try:
-        if config.list:
-            list_hunters()
-            return
-
-        if not any(scan_options):
-            if not interactive_set_config(): return
-
-        with hunt_started_lock:
-            hunt_started = True
-        handler.publish_event(HuntStarted())
-        if config.pod:
-            handler.publish_event(RunningAsPodEvent())
-        else:
-            handler.publish_event(HostScanEvent())
-
-        # Blocking to see discovery output
-        handler.join()
-    except KeyboardInterrupt:
-        logging.debug("Kube-Hunter stopped by user")
-    # happens when running a container without interactive option
-    except EOFError:
-        logging.error("\033[0;31mPlease run again with -it\033[0m")
-    finally:
-        hunt_started_lock.acquire()
-        if hunt_started:
-            hunt_started_lock.release()
-            handler.publish_event(HuntFinished())
-            handler.join()
-            handler.free()
-            logging.debug("Cleaned Queue")
-        else:
-            hunt_started_lock.release()
-
-
-if __name__ == '__main__':
-        main()
+kube_hunter/__main__.py

kube_hunter/__main__.py
@@ -1,0 +1,130 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import threading
+
+from kube_hunter.conf import config
+from kube_hunter.modules.report.plain import PlainReporter
+from kube_hunter.modules.report.yaml import YAMLReporter
+from kube_hunter.modules.report.json import JSONReporter
+from kube_hunter.modules.report.dispatchers import STDOUTDispatcher, HTTPDispatcher
+from kube_hunter.core.events import handler
+from kube_hunter.core.events.types import HuntFinished, HuntStarted
+from kube_hunter.modules.discovery.hosts import RunningAsPodEvent, HostScanEvent
+
+
+loglevel = getattr(logging, config.log.upper(), logging.INFO)
+
+if config.log.lower() != "none":
+    logging.basicConfig(level=loglevel, format='%(message)s', datefmt='%H:%M:%S')
+
+reporters = {
+    'yaml': YAMLReporter,
+    'json': JSONReporter,
+    'plain': PlainReporter
+}
+
+if config.report.lower() in reporters.keys():
+    config.reporter = reporters[config.report.lower()]()
+else:
+    logging.warning('Unknown reporter selected, using plain')
+    config.reporter = reporters['plain']()
+
+dispatchers = {
+    'stdout': STDOUTDispatcher,
+    'http': HTTPDispatcher
+}
+
+if config.dispatch.lower() in dispatchers.keys():
+    config.dispatcher = dispatchers[config.dispatch.lower()]()
+else:
+    logging.warning('Unknown dispatcher selected, using stdout')
+    config.dispatcher = dispatchers['stdout']()
+
+import kube_hunter
+
+
+def interactive_set_config():
+    """Sets config manually, returns True for success"""
+    options = [("Remote scanning", "scans one or more specific IPs or DNS names"),
+    ("Interface scanning","scans subnets on all local network interfaces"),
+    ("IP range scanning","scans a given IP range")]
+
+    print("Choose one of the options below:")
+    for i, (option, explanation) in enumerate(options):
+        print("{}. {} ({})".format(i+1, option.ljust(20), explanation))
+    choice = input("Your choice: ")
+    if choice == '1':
+        config.remote = input("Remotes (separated by a ','): ").replace(' ', '').split(',')
+    elif choice == '2':
+        config.interface = True
+    elif choice == '3':
+        config.cidr = input("CIDR (example - 192.168.1.0/24): ").replace(' ', '')
+    else:
+        return False
+    return True
+
+
+def list_hunters():
+    print("\nPassive Hunters:\n----------------")
+    for hunter, docs in handler.passive_hunters.items():
+        name, doc = hunter.parse_docs(docs)
+        print("* {}\n  {}\n".format(name, doc))
+
+    if config.active:
+        print("\n\nActive Hunters:\n---------------")
+        for hunter, docs in handler.active_hunters.items():
+            name, doc = hunter.parse_docs(docs)
+            print("* {}\n  {}\n".format( name, doc))
+
+
+global hunt_started_lock
+hunt_started_lock = threading.Lock()
+hunt_started = False
+
+def main():
+    global hunt_started
+    scan_options = [
+        config.pod,
+        config.cidr,
+        config.remote,
+        config.interface
+    ]
+    try:
+        if config.list:
+            list_hunters()
+            return
+
+        if not any(scan_options):
+            if not interactive_set_config(): return
+
+        with hunt_started_lock:
+            hunt_started = True
+        handler.publish_event(HuntStarted())
+        if config.pod:
+            handler.publish_event(RunningAsPodEvent())
+        else:
+            handler.publish_event(HostScanEvent())
+
+        # Blocking to see discovery output
+        handler.join()
+    except KeyboardInterrupt:
+        logging.debug("Kube-Hunter stopped by user")
+    # happens when running a container without interactive option
+    except EOFError:
+        logging.error("\033[0;31mPlease run again with -it\033[0m")
+    finally:
+        hunt_started_lock.acquire()
+        if hunt_started:
+            hunt_started_lock.release()
+            handler.publish_event(HuntFinished())
+            handler.join()
+            handler.free()
+            logging.debug("Cleaned Queue")
+        else:
+            hunt_started_lock.release()
+
+
+if __name__ == '__main__':
+        main()

setup.cfg
@@ -1,3 +1,56 @@
+[metadata]
+name = kube-hunter
+description = Kubernetes security weaknesses hunter for humans
+long_description = file: README.md
+long_description_content_type = text/markdown
+author = Aqua Security
+author_email = support@aquasec.com
+url = https://github.com/aquasecurity/kube-hunter
+keywords =
+    aquasec
+    hunter
+    kubernetes
+    k8s
+    security
+license_file = LICENSE
+classifiers =
+    Development Status :: 4 - Beta
+    Environment :: Console
+    License :: OSI Approved :: Apache Software License
+    Natural Language :: English
+    Operating System :: OS Independent
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+    Topic :: Security
+
+[options]
+zip_safe = False
+packages = find:
+install_requires =
+    netaddr
+    netifaces
+    scapy>=2.4.3
+    requests
+    PrettyTable
+    urllib3>=1.24.2,<1.25
+    ruamel.yaml
+    requests_mock
+    future
+    packaging
+setup_requires =
+    setuptools>=30.3.0
+    setuptools_scm
+test_requires =
+    pytest>=2.9.1
+    coverage<5.0
+    pytest-cov
+python_requires = >=3.6
+
+[options.entry_points]
+console_scripts =
+    kube-hunter = kube_hunter.__main__:main
+
 [aliases]
 test=pytest
 
@@ -33,3 +86,7 @@ exclude_lines =
     if __name__ == .__main__.:
     # Don't complain about log messages not being tested
     logging\.
+
+# Files to exclude from consideration
+omit =
+    kube_hunter/__main__.py

setup.py
@@ -1,0 +1,5 @@
+from setuptools import setup
+
+setup(
+    use_scm_version=True,
+)

commit e3b76ee793fac597692652fa916d93a82979acb2

Fix Dockerfile build
The Docker build used a 2-step installation of requirements
and application.
This was broke by #272.

Fixes #300

.dockerignore
@@ -1,0 +1,4 @@
+*.png
+tests/
+docs/
+.github/

.gitignore
@@ -1,6 +1,5 @@
 *.pyc
 .venv
-.dockerignore
 *aqua*
 venv/
 .vscode

Dockerfile
@@ -1,25 +1,16 @@
-FROM python:3.7-alpine3.10 as builder
+FROM python:3.8-alpine as builder
 
 RUN apk add --no-cache \
     linux-headers \
     tcpdump \
     build-base \
-    ebtables
+    ebtables \
+    make \
+    git && \
+    apk upgrade --no-cache
 
 WORKDIR /kube-hunter
-COPY ./requirements.txt /kube-hunter/.
-RUN pip install -r /kube-hunter/requirements.txt -t /kube-hunter
+COPY . .
+RUN make install
 
-COPY . /kube-hunter
-
-FROM python:3.7-alpine3.10
-
-RUN apk add --no-cache \
-    tcpdump
-RUN apk upgrade --no-cache 
-
-COPY --from=builder /kube-hunter /kube-hunter
-
-WORKDIR /kube-hunter
-
-ENTRYPOINT ["python",  "kube-hunter.py"]
+ENTRYPOINT ["kube-hunter"]