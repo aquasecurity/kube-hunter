f7fe0e4d4cf2433cfe01b2eb5fed9304eac3bf31
mormamn <61231073+mormamn@users.noreply.github.com>
2020-03-04 20:37:43 +0200

    Update kube_hunter/modules/hunting/secrets.py
    Co-Authored-By: Yehuda Chikvashvili <yehudaac1@gmail.com>

--- kube_hunter/modules/hunting/secrets.py
+++ kube_hunter/modules/hunting/secrets.py
@@ -37,8 +37,7 @@ class AccessSecrets(Hunter):
         self.secrets_evidence = ''
 
     def get_services(self):
-        logger.debug(self.event.host)
-        logger.debug('Passive Hunter is attempting to access pod\'s secrets directory')
+        logger.debug("Trying to access pod's secrets directory")
         # get all files and subdirectories files:
         self.secrets_evidence = [val for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk('/var/run/secrets/')] for val in sublist]
         return True if (len(self.secrets_evidence) > 0) else False
