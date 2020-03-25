05f37df4d6ab277e7d872b90d42d11f0806c9135
yoavrotems <yoavrotems97@gmail.com>
2020-03-25 17:22:02 +0200

    Create test___init__.py
    testing here because having problem with local pytest

---
+++ tests/conf/test___init__.py
@@ -0,0 +1,13 @@
+from kube_hunter.conf.__init__ import logging,setup_logger
+
+def test_setup_logger():
+    test_cases = [
+        ("NONE", 50),
+        ("INFO", 20),
+        ("DEBUG", 10),
+        ("CRITICAL", 50)
+        ("GIBRISH", 20)
+    ]
+    for test_log_level_name, expected in test_cases:
+        setup_logger(test_log_level_name)
+        assert type(logging.getLogger().getEffectiveLevel()) is expected
