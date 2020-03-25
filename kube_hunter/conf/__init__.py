aea1a168ee6b313ddffe8202a21752053734faa7
yoavrotems <yoavrotems97@gmail.com>
2020-03-25 17:20:00 +0200

    Update __init__.py

--- kube_hunter/conf/__init__.py
+++ kube_hunter/conf/__init__.py
@@ -2,17 +2,19 @@ import logging
 
 from kube_hunter.conf.parser import parse_args
 
+def setup_logger(log_level_name):
+    formatter = '%(asctime)s %(levelname)s %(name)s %(message)s'
+    if log_level_name == "NONE":
+        logging.disable(logging.CRITICAL)
+    else:
+        log_level = getattr(logging, log_level_name, None)
+        if not log_level:
+            log_level = logging.INFO
+            logging.warning("Unknown log level selected, using INFO")
+        logging.basicConfig(level=log_level_name, format=formatter)
+
 config = parse_args()
-formatter = '%(asctime)s %(levelname)s %(name)s %(message)s'
-
-loglevel = getattr(logging, config.log.upper(), None)
-
-if not loglevel:
-    logging.basicConfig(level=logging.INFO,
-                        format=formatter)
-    logging.warning('Unknown log level selected, using info')
-elif config.log.lower() != "none":
-    logging.basicConfig(level=loglevel,
-                        format=formatter)
+log_level_name = config.log.upper()
+setup_logger(log_level_name)
 
 import plugins
