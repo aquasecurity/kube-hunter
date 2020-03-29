b640ce47d449f93e21ba952cfd04e8bbdbd9617f
shellw <shelley.wu@getcruise.com>
2020-03-28 21:12:52 -0700

    Add prefix function

--- kube_hunter/modules/report/ndjson.py
+++ kube_hunter/modules/report/ndjson.py
@@ -14,53 +14,39 @@ class NDJSONReporter(BaseReporter):
         for node in nodes:
             nodeAdded = False
             node_location = node["location"]
-            node_type = node["type"]
             for service in services:
                 serviceAdded = False
-                service_service = service["service"]
                 service_location = service["location"]
-                service_description = service["description"]
                 service_ip = service["location"].split(":")[0]
                 if service_ip == node_location:
                     for vulnerability in vulnerabilities:
                         vulnerability_location = vulnerability["location"]
-                        vulnerability_vid = vulnerability["vid"]
-                        vulnerability_category = vulnerability["category"]
-                        vulnerability_severity = vulnerability["severity"]
-                        vulnerability_vulnerability = vulnerability["vulnerability"]
-                        vulnerability_description = vulnerability["description"]
-                        vulnerability_evidence = vulnerability["evidence"]
-                        vulnerability_hunter = vulnerability["hunter"]
                         if vulnerability_location == service_location:
-                            entry = create_entry(node_location, node_type, service_service, service_location, service_description, vulnerability_location, vulnerability_vid, vulnerability_category, vulnerability_severity, vulnerability_vulnerability, vulnerability_description, vulnerability_evidence, vulnerability_hunter)
+                            entry = create_entry(node, service, vulnerability)
                             flattenedEntries.append(entry)
                             nodeAdded = True
                             serviceAdded = True
                     if not serviceAdded:
-                        entry = create_entry(node_location, node_type, service_service, service_location, service_description)
+                        entry = create_entry(node, service)
                         flattenedEntries.append(entry)
                         nodeAdded = True
             if not nodeAdded:
-                entry = create_entry(node_location, node_type, service_service, service_location, service_description)
+                entry = create_entry(node)
                 flattenedEntries.append(entry)
         for logEntry in flattenedEntries:
             print(json.dumps(logEntry))
         return
 
 
-def create_entry(node_location="", node_type="", service_service="", service_location="", service_description="", vulnerability_location="", vulnerability_vid="", vulnerability_category="", vulnerability_severity="", vulnerability_vulnerability="", vulnerability_description="", vulnerability_evidence="", vulnerability_hunter=""):
+def create_entry(node={}, service={}, vulnerability={}):
     flattenedEntry = {}
-    flattenedEntry["node_location"] = node_location
-    flattenedEntry["node_type"] = node_type
-    flattenedEntry["service_service"] = service_service
-    flattenedEntry["service_location"] = service_location
-    flattenedEntry["service_description"] = service_description
-    flattenedEntry["vulnerability_location"] = vulnerability_location
-    flattenedEntry["vulnerability_vid"] = vulnerability_vid
-    flattenedEntry["vulnerability_category"] = vulnerability_category
-    flattenedEntry["vulnerability_severity"] = vulnerability_severity
-    flattenedEntry["vulnerability_vulnerability"] = vulnerability_vulnerability
-    flattenedEntry["vulnerability_description"] = vulnerability_description
-    flattenedEntry["vulnerability_evidence"] = vulnerability_evidence
-    flattenedEntry["vulnerability_hunter"] = vulnerability_hunter
+    prefix_dict_keys(node, "node", flattenedEntry)
+    prefix_dict_keys(service, "service", flattenedEntry)
+    prefix_dict_keys(vulnerability, "vulnerability", flattenedEntry)
     return flattenedEntry
+
+
+def prefix_dict_keys(data, prefix, entry):
+    for key in data.keys():
+        prefixed_key = prefix + "_" + key
+        entry[prefixed_key] = data[key]
