15fc11bef8f586f5b8f11bc68eee66b796648f5f
yoavrotems <yoavrotems97@gmail.com>
2020-04-01 01:51:00 +0300

    Update __init__.py
    Changed a few things to be more neat, also deleting any old logging handlers and moved it into a function in order to unit test it

---
+++ kube_hunter/conf/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+
+DEFAULT_LEVEL = logging.DEBUG
+DEFAULT_LEVEL_NAME = logging.getLevelName(DEFAULT_LEVEL)
+LOG_FORMAT = "%(asctime)s %(levelname)s %(name)s %(message)s"
+
+
+def setup_logger(level_name):
+    # Remove any existing handlers
+    # Unnecessary in Python 3.8 since `logging.basicConfig` has `force` parameter
+    for h in logging.getLogger().handlers[:]:
+        h.close()
+        logging.getLogger().removeHandler(h)
+
+    if level_name == "NONE":
+        logging.disable(logging.CRITICAL)
+    else:
+        log_level = getattr(logging._nameToLevel, level_name, None)
+        logging.basicConfig(level=log_level or DEFAULT_LEVEL, format=LOG_FORMAT)
+        if not log_level:
+            logging.warning(f"Unknown log level selected, using {DEFAULT_LEVEL_NAME}")
