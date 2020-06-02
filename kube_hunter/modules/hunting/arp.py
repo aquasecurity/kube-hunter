974f415e5449977ec5666b2fbaddb108c32b737d
Konstantin Weddige <kontakt@weddige.eu>
2020-06-02 09:13:03 +0200

    Fix typo
    This fixes #353

--- kube_hunter/modules/hunting/arp.py
+++ kube_hunter/modules/hunting/arp.py
@@ -55,7 +55,7 @@ class ArpSpoofHunter(ActiveHunter):
         config = get_config()
         self_ip = sr1(IP(dst="1.1.1.1", ttl=1) / ICMP(), verbose=0, timeout=config.network_timeout)[IP].dst
         arp_responses, _ = srp(
-            Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=f"{self_ip}/24"), timeout=config.netork_timeout, verbose=0,
+            Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=f"{self_ip}/24"), timeout=config.network_timeout, verbose=0,
         )
 
         # arp enabled on cluster and more than one pod on node
