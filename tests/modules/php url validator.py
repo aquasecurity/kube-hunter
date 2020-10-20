585d31f3a4b5e4eb8da867758d8c233b921667ee
Oshada Lahiru <56959964+heaboyhela@users.noreply.github.com>
2020-10-20 18:44:15 +0530

    php url validator.py
    php url validator.py

---
+++ tests/modules/php url validator.py
@@ -0,0 +1,5 @@
+$website = input($_POST["site"]);
+
+if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+   $websiteErr = "Invalid URL"; 
+}
