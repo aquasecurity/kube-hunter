b4643804a7f67f96e7eedf83d58885eadd2b3b82
Mikolaj Pawlikowski <mpawlikowsk1@bloomberg.net>
2021-05-07 12:39:39 +0100

    Add tests to cover the k8s node listing

--- kube_hunter/conf/k8s.py
+++ kube_hunter/conf/k8s.py
@@ -1,26 +1,27 @@
 import logging
-from kubernetes import client, config
+import kubernetes
 
 
-def list_all_k8s_cluster_nodes(kube_config=None):
+def list_all_k8s_cluster_nodes(kube_config=None, client=None):
     logger = logging.getLogger(__name__)
     try:
         if kube_config:
             logger.info("Attempting to use kubeconfig file: %s", kube_config)
-            config.load_kube_config(config_file=kube_config)
+            kubernetes.config.load_kube_config(config_file=kube_config)
         else:
             logger.info("Attempting to use in cluster Kubernetes config")
-            config.load_incluster_config()
-    except config.config_exception.ConfigException:
+            kubernetes.config.load_incluster_config()
+    except kubernetes.config.config_exception.ConfigException:
         logger.exception("Failed to initiate Kubernetes client")
         return
 
     try:
-        ret = client.CoreV1Api().list_node(watch=False)
+        if client is None:
+            client = kubernetes.client.CoreV1Api()
+        ret = client.list_node(watch=False)
         logger.info("Listed %d nodes in the cluster" % len(ret.items))
         for item in ret.items:
-            addresses = item.status.addresses
-            for addr in addresses:
+            for addr in item.status.addresses:
                 yield addr.address
     except:
         logger.exception("Failed to list nodes from Kubernetes")
