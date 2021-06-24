75a7abe8382f3c21de1bb6bdf71c80a7532fd7ef
Daniel Sagi <daniel.sagi@aquasec.com>
2021-06-24 18:31:29 +0300

    fixed linting with exceptions

--- tests/discovery/test_k8s.py
+++ tests/discovery/test_k8s.py
@@ -1,9 +1,9 @@
 from kube_hunter.conf import Config, set_config
 
-set_config(Config())
-
 from kube_hunter.modules.discovery.kubernetes_client import list_all_k8s_cluster_nodes
 from unittest.mock import MagicMock, patch
+
+set_config(Config())
 
 
 def test_client_yields_ips():
